7aa15bde604d15e7884ac851fa2329d2fe0150b6
kyawswaraye <phoepyone.tgi@gmail.com>
2023-02-12 00:44:18 +0630

    Fixed:navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,27 +20,37 @@ function MobileNav({ open, setOpen }) {
             <div className='z-20 space-y-6 grid place-items-center'>
                 <div className='mt-12'>
                     <Link href="/">
-                        <a onClick={handleClick} className={menuClass('/')}>Home</a>
+                        <a onClick={handleClick} className={menuClass('/')}>
+                            <p className="text-sm">Home</p>
+                        </a>
                     </Link>
                 </div>
                 <div>
                     <Link href="/products">
-                        <a onClick={handleClick} className={menuClass('/products')}>Products</a>
+                        <a onClick={handleClick} className={menuClass('/products')}>
+                            <p className="text-sm">Products</p>
+                        </a>
                     </Link>
                 </div>
                 <div>
                     <Link href="/contact">
-                        <a onClick={handleClick} className={menuClass('/contact')}>Contact</a>
+                        <a onClick={handleClick} className={menuClass('/contact')}>
+                            <p className='text-sm'>Contact</p>
+                        </a>
                     </Link>
                 </div>
                 <div>
                     <Link href="/about">
-                        <a onClick={handleClick} className={menuClass('/about')}>About</a>
+                        <a onClick={handleClick} className={menuClass('/about')}>
+                            <p className="text-sm">About</p>
+                        </a>
                     </Link>
                 </div>
                 <div>
                     <Link href="/retailers">
-                        <a onClick={handleClick} className={menuClass('/retailers')}>Retailers</a>
+                        <a onClick={handleClick} className={menuClass('/retailers')}>
+                            <p className="text-sm">Retailers</p>
+                        </a>
                     </Link>
                 </div>
                 <div className='flex items-center justify-center'>
@@ -55,16 +65,16 @@ export default function Navbar() {
     const Router = useRouter()
     const [open, setOpen] = useState(false)
     const navMenuClass = (path) => {
-        const activeClass = Router.pathname === path ? "text-yellow-400 tracking-widest mx-4" : "tracking-widest mx-4";
+        const activeClass = Router.pathname === path ? "text-yellow-400 tracking-widest px-2" : "tracking-widest px-2";
         return activeClass + ` hover:text-color-secondary`;
     }
     return (
-        <nav className="sticky top-0 z-30 flex filter drop-shadow-md bg-light-primary text-gray-300 dark:bg-secondary px-8 h-28 items-center">
+        <nav className="sticky top-0 z-30 flex filter drop-shadow-md bg-light-primary text-gray-300 dark:bg-secondary px-8 h-18 items-center">
             <MobileNav open={open} setOpen={setOpen} />
             <div className="w-3/12">
                 <Link href={"/"}>
                     <a>
-                        <Image src={"/images/logo.png"} alt={"ounpha-logo"} width={175} height={75} objectFit="cover" />
+                        <Image src={"/images/logo.png"} alt={"ounpha-logo"} width={100} height={70} objectFit="contain" />
                     </a>
                 </Link>
             </div>
@@ -75,27 +85,37 @@ export default function Navbar() {
                     setOpen(!open)
                 }}>
                     {/* hamburger button */}
-                    <span className={`h-1 w-full bg-secondary rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
-                    <span className={`h-1 w-full bg-secondary rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
-                    <span className={`h-1 w-full bg-secondary rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+                    <span className={`h-1 w-full bg-gray-400 rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+                    <span className={`h-1 w-full bg-gray-400 rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+                    <span className={`h-1 w-full bg-gray-400 rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
                 </div>
             </div>
 
             <div className="w-full hidden md:flex text-gray-100 items-center justify-end" >
                 <Link href="/">
-                    <a className={navMenuClass('/')}>Home</a>
+                    <a className={navMenuClass('/')}>
+                        <p className="text-xs">Home</p>
+                    </a>
                 </Link>
                 <Link href="/products">
-                    <a className={navMenuClass('/products')}>Products</a>
+                    <a className={navMenuClass('/products')}>
+                        <p className="text-xs">Products</p>
+                    </a>
                 </Link>
                 <Link href="/retailers">
-                    <a className={navMenuClass('/retailers')}>Retailers</a>
+                    <a className={navMenuClass('/retailers')}>
+                        <p className="text-xs">Retailers</p>
+                    </a>
                 </Link>
                 <Link href="/contact">
-                    <a className={navMenuClass('/contact')}>Contact</a>
+                    <a className={navMenuClass('/contact')}>
+                        <p className="text-xs">Contact</p>
+                    </a>
                 </Link>
                 <Link href="/about">
-                    <a className={navMenuClass('/about')}>About</a>
+                    <a className={navMenuClass('/about')}>
+                        <p className="text-xs">About</p>
+                    </a>
                 </Link>
                 <DisplayDarkMode />
             </div>
